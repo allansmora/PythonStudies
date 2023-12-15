f96c5be59bf52a54e3b265ad94b9ace57bf52f92
Allan Moura <anuikpom@gmail.com>
2023-12-15 13:00:41 -0300

    Novos comentários

--- Projects/ConverterImagensEmPdf.py
+++ Projects/ConverterImagensEmPdf.py
@@ -1,15 +1,15 @@
 import os
 import img2pdf
 
-# Replace the directory path with the folder containing JPEG images to be converted
+# Diretório onde estão as imagens
 directory_path = "C:\\Users\\Allan\\Documents\\GitHub\\PythonStudies\\Projects"
 
-# List all files in the directory and filter only JPEG images (ending with ".jpg")
+# Busca apenas os arquivos do tipo JPEG
 image_files = [i for i in os.listdir(directory_path) if i.endswith(".jpg")]
 
-# Convert the list of JPEG images to a single PDF file
+# Converte as imagens em PDF
 pdf_data = img2pdf.convert(image_files)
 
-# Write the PDF content to a file (make sure you have write permissions for the specified file)
+# Coloca o conteúdo no pdf
 with open("output.pdf", "wb") as file:
     file.write(pdf_data)
